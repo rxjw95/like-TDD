0757bbfe1f9fa2793b718905ea4bc467cb525db6
Jangwook Ryu <rxjw95@gmail.com>
2022-10-23 02:00:23 +0900

    TDD - Red
    - 요구사항 변경 (값이 오름차순으로 변경)

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -9,8 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoTest {
 
     @Test
-    void print_6개의_서로_다정_숫자가_공백으로_분리된_문자열로_반환한다() {
-        Lotto lotto = new Lotto(List.of(1, 4, 12, 23, 34, 40));
+    void print_6개의_서로_다른_오름차순_숫자가_공백으로_분리된_문자열로_반환한다() {
+        Lotto lotto = new Lotto(List.of(1, 12, 4, 23, 34, 40));
         String print = lotto.print();
 
         assertThat(print).isEqualTo("1 4 12 23 34 40");
