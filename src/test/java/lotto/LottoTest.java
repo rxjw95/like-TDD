350121f510d31258311657043a994e01f3032137
Jangwook Ryu <rxjw95@gmail.com>
2022-10-23 01:58:52 +0900

    TDD - Green
    - List.of()의 정수를 다른 값으로 지정하면 테스트 실패
    - 알맞게 코드 수정

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -9,10 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoTest {
 
     @Test
-    void print_6개의_서로_다른_숫자가_공백으로_분리된_문자열로_반환한다() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    void print_6개의_서로_다정_숫자가_공백으로_분리된_문자열로_반환한다() {
+        Lotto lotto = new Lotto(List.of(1, 4, 12, 23, 34, 40));
         String print = lotto.print();
 
-        assertThat(print).isEqualTo("1 2 3 4 5 6");
+        assertThat(print).isEqualTo("1 4 12 23 34 40");
     }
 }
