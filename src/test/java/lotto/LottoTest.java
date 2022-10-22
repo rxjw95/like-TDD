e32019b8805f42de458b1d9b6aa34bc0063ee6d2
Jangwook Ryu <rxjw95@gmail.com>
2022-10-23 01:49:24 +0900

    TDD - Red
    - 실패하는 테스트 코드를 작성하라

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -2,9 +2,7 @@ package lotto;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,10 +10,9 @@ public class LottoTest {
 
     @Test
     void print_6개의_서로_다른_숫자가_공백으로_분리된_문자열로_반환한다() {
-        Lotto lotto = new Lotto();
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         String print = lotto.print();
 
-        Set<Integer> set = Arrays.stream(print.split(" ")).map(Integer::parseInt).collect(Collectors.toSet());
-        assertThat(set.size()).isEqualTo(6);
+        assertThat(print).isEqualTo("1 2 3 4 5 6");
     }
 }
