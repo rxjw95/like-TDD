12de66396df93868fe963a1df676b624e129e188
Jangwook Ryu <rxjw95@gmail.com>
2022-10-23 02:12:41 +0900

    TDD - Red
    - 실패하는 테스트 코드를 작성하라

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoGeneratorTest {
+
+    @Test
+    void generate_난수가_6개인_Lotto를_생성한다() {
+        LottoGenerator generator = LottoGenerator();
+        Lotto lotto1 = generator.generate();
+        Lotto lotto2 = generator.generate();
+
+        assertThat(lotto1.print()).isNotEqualTo(lotto2);
+    }
+
+}
